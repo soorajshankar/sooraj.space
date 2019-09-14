216a3fae664936f3c1ac666513210ad3b082becb
Sooraj <Sooraj.sanker@itcinfotech.com>
2019-09-15 01:16:29 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+## Personal Portfolio made using GatsbyJS 
---
+++ config.js
@@ -0,0 +1,41 @@
+"use strict";
+
+module.exports = {
+  url: "https://sooraj.space",
+  pathPrefix: "/",
+  title: "Sooraj's Space",
+  subtitle: "Web Maker, FrontEnd Engineer, IoT Designer",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
+  postsPerPage: 4,
+  googleAnalyticsId: "",
+  useKatex: false,
+  menu: [
+    {
+      label: "Articles",
+      path: "/"
+    },
+    {
+      label: "About me",
+      path: "/pages/about"
+    }
+  ],
+  author: {
+    name: "Sooraj Sanker",
+    photo: "/photo.jpg",
+    bio: "Work for Web Performance, Smooth FrontEnd Experience, Beautiful Mobile Applications, Secure IoT Systems",
+    contacts: {
+      email: "soorajshankar@gmail.com",
+      telegram: "soorajshankar",
+      twitter: "soorajsanker",
+      github: "soorajshankar",
+      rss: "",
+      vkontakte: "",
+      linkedin: "sooraj-sanker-41a51144/",
+      instagram: "soorajshankar",
+      line: "",
+      gitlab: "",
+      weibo: ""
+    }
+  }
+};
