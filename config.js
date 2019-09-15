7dc372faf047ecf6b69b0e7cf657f447e9c5734a
Sooraj <soorajshankar@gmail.com>
2019-09-15 13:30:03 +0530

    GA tracking ID

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   copyright: "© All rights reserved.",
   disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: "210208845",
+  googleAnalyticsId: "UA-33079947-10",
   useKatex: false,
   menu: [
     {
