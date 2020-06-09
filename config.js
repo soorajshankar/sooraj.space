6c3ae9103687fcf37abe58484286c8bc5bb2d175
soorajshankar <soorajshankar@gmail.com>
2020-06-09 23:44:51 +0530

    config update

--- config.js
+++ config.js
@@ -1,46 +1,48 @@
-"use strict";
+'use strict';
 
 module.exports = {
-  url: "https://sooraj.space",
-  pathPrefix: "/",
-  title: "Sooraj's Space",
-  subtitle: "Web Maker, FrontEnd Engineer, IoT Designer",
-  copyright: "2020 © All rights reserved.",
-  disqusShortname: "sooraj-space",
+  url: 'https://sooraj.space',
+  pathPrefix: '/',
+  title: 'Sooraj\'s Space',
+  subtitle: 'Web Maker, FrontEnd Engineer, IoT Designer',
+  copyright: '2020 © All rights reserved.',
+  disqusShortname: 'sooraj-space',
   postsPerPage: 4,
-  googleAnalyticsId: "UA-33079947-10",
+  googleAnalyticsId: 'UA-33079947-10',
   useKatex: false,
   menu: [
     {
-      label: "Articles",
-      path: "/",
+      label: 'Articles',
+      path: '/',
     },
     {
-      label: "About me",
-      path: "/pages/about",
+      label: 'About me',
+      path: '/pages/about',
     },
     {
-      label: "Public Talks",
-      path: "/talks",
+      label: 'Public Talks',
+      path: '/talks',
     },
   ],
   author: {
-    name: "Sooraj Sanker",
-    photo: "/photo.jpg",
+    name: 'Sooraj Sanker',
+    photo: '/photo.jpg',
     bio:
-      "Work for Web Performance, Smooth FrontEnd Experience, Beautiful Mobile Applications, Secure IoT Systems",
+      'Work for Web Performance, Smooth FrontEnd Experience, Beautiful Mobile Applications, Secure IoT Systems',
     contacts: {
-      email: "soorajshankar@gmail.com",
-      telegram: "soorajshankar",
-      twitter: "soorajsanker",
-      github: "soorajshankar",
-      rss: "",
-      vkontakte: "",
-      linkedin: "sooraj-sanker-41a51144/",
-      instagram: "soorajshankar",
-      line: "",
-      gitlab: "",
-      weibo: "",
-    },
-  },
+      email: 'soorajshankar@gmail.com',
+      telegram: 'soorajshankar',
+      twitter: 'soorajsanker',
+      github: 'soorajshankar',
+      facebook: '#',
+      rss: '',
+      vkontakte: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
 };
